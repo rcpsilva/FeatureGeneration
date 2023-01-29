d027f607cae79c0d127ac34922b608ed32ce90be
rcpsilva <rcpsilva@gmail.com>
2023-01-29 01:58:36 -0300

    Full experiments for continuous features

---
+++ experiments.py
@@ -0,0 +1,72 @@
+import feature_creator as fc
+import pandas as pd
+import xgboost as xgb
+from xgboost.sklearn import XGBClassifier
+from sklearn.model_selection import train_test_split,StratifiedShuffleSplit,cross_val_score
+from sklearn.base import clone
+import numpy as np
+from copy import copy,deepcopy
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+import pickle
+from tqdm import tqdm
+
+X_data = []
+y_data = []
+
+df = pd.read_csv('spambase.csv')
+X = df.drop(columns=['spam']).to_numpy()
+y = df['spam'].to_numpy()
+
+X_data.append(deepcopy(X))
+y_data.append(deepcopy(y))
+
+df = pd.read_csv('spect_train.csv')
+X = df.drop(columns=['OVERALL_DIAGNOSIS']).to_numpy()
+y = df['OVERALL_DIAGNOSIS'].to_numpy()
+
+X_data.append(deepcopy(X))
+y_data.append(deepcopy(y))
+
+df = pd.read_csv('ionosphere_data.csv')
+X = df.drop(columns=['column_ai']).to_numpy()
+y = df['column_ai'].to_numpy()
+
+X_data.append(deepcopy(X))
+y_data.append(deepcopy(y))
+
+model = XGBClassifier()
+
+feature_models = [XGBClassifier(),DecisionTreeRegressor(max_depth=5),DecisionTreeRegressor(max_depth=3)]
+n_features = [1,2,3,4,5]
+batch_sizes = [0.1,0.2,0.3]
+
+results = []
+
+for i in tqdm(range(len(y_data))):
+    for fm in tqdm(feature_models):
+        for n in tqdm(n_features):
+            for b in tqdm(batch_sizes):
+                features, ga_fitness = fc.feature_creator(model,
+                                            fm,
+                                            X_data[i],
+                                            y_data[i],
+                                            n_features=n,
+                                            batch_size=b)
+
+                new_X = copy(X)
+                for f in features:
+                    new_X = np.hstack((new_X,np.array([fc.get_feature_values(f,X)]).T))
+
+                base_scores = cross_val_score(model, X, y, cv=10, scoring='f1')
+                scores = cross_val_score(model, new_X, y, cv=10, scoring='f1')
+
+                row = [i,fm.__str__()[:15],n,b,np.mean(base_scores),np.std(base_scores),np.mean(scores),np.std(scores)]
+                results.append(row)
+
+                with open('results_continuous_fetures_2023_01_29.pkl', 'wb') as f:
+                    pickle.dump(results, f)
+
+
+
+print(results)
