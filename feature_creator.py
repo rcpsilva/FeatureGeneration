0829767775027a9e2a2723c28a0ec9a8b7f93249
rcpsilva <rcpsilva@gmail.com>
2023-02-07 12:04:08 -0300

    Performed preliminary experiments with Logistic Regression and Powell's Method

--- feature_creator.py
+++ feature_creator.py
@@ -29,12 +29,12 @@ def feature_creator(model,feature_model,X,y,n_features=5,batch_size=0.05):
         fitness_function = lambda x: -fitness(x,X_temp,y_temp,model)
 
         # Nelder mead
-        #bounds = Bounds(np.zeros(len(y_temp)), np.ones(len(y_temp)))
+        bounds = Bounds(np.zeros(len(y_temp)), np.ones(len(y_temp)))
         x0 = np.random.rand(len(y_temp)) # Initial guess for the optimization
         options = {'maxiter': 1000, 'xatol': 1e-2, 'fatol': 1e-2}
-        res = minimize(fitness_function, x0, method='Nelder-Mead',options=options)
+        #res = minimize(fitness_function, x0, method='Nelder-Mead',options=options)
 
-        #res = minimize(fitness_function, x0, method='Powell',options=options,bounds=bounds)
+        res = minimize(fitness_function, x0, method='Powell',options=options,bounds=bounds)
 
         #bounds = Bounds(np.zeros(len(y_temp)), np.ones(len(y_temp)))
         #res = shgo(fitness_function, bounds)
